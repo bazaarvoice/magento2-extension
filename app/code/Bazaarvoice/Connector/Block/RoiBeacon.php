aa2f1e22e18ed8c1fb09e70dafc92e9710c95262
Dennis Rogers <dennis@storefrontconsulting.com>
2016-04-01 16:33:48 -0400

    Merge branch 'initial' of /Users/dennis/Sites/vmware/bvmage2 with conflicts.

---
+++ app/code/Bazaarvoice/Connector/Block/RoiBeacon.php
@@ -0,0 +1,112 @@
+<?php
+/**
+ * NOTICE OF LICENSE
+ *
+ * This source file is subject to commercial source code license
+ * of StoreFront Consulting, Inc.
+ *
+ * @copyright    (C)Copyright 2016 StoreFront Consulting, Inc (http://www.StoreFrontConsulting.com/)
+ * @package      Bazaarvoice_Connector
+ * @author       Dennis Rogers <dennis@storefrontconsulting.com>
+ */
+
+namespace Bazaarvoice\Connector\Block;
+
+class RoiBeacon
+{
+
+    /**
+     * @param \Bazaarvoice\Connector\Helper\Data $helper
+     * @param \Magento\Checkout\Model\Session $checkoutSession
+     * @param \Magento\Framework\ObjectManagerInterface $objectManager
+     */
+    public function __construct(
+        \Bazaarvoice\Connector\Helper\Data $helper,
+        \Magento\Checkout\Model\Session $checkoutSession,
+        \Magento\Framework\ObjectManagerInterface $objectManager
+    ) {
+        $this->helper = $helper;
+        $this->checkoutSession = $checkoutSession;
+        $this->objectManager = $objectManager;
+    }
+
+    public function afterToHtml($subject, $result) {
+        if($this->helper->getConfig('general/enable_roibeacon') != true)
+            return $result;
+
+        /** @var \Magento\Sales\Model\Order $order */
+        $order = $this->checkoutSession->getLastRealOrder();
+
+        $orderDetails = array();
+
+        $address = $order->getBillingAddress();
+
+        $orderDetails['orderId'] = $order->getIncrementId();
+        $orderDetails['tax'] = number_format($order->getTaxAmount(), 2, '.', '');
+        $orderDetails['shipping'] = number_format($order->getShippingAmount(), 2, '.', '');
+        $orderDetails['total'] = number_format($order->getGrandTotal(), 2, '.', '');
+        $orderDetails['city'] = $address->getCity();
+        /** @var \Magento\Directory\Model\Region $region */
+        $region = $this->objectManager->get('\Magento\Directory\Model\Region');
+        $orderDetails['state'] = $region->load($address->getRegionId())->getCode();
+        $orderDetails['country'] = $address->getCountryId();
+        $orderDetails['currency'] = $order->getOrderCurrencyCode();
+
+        $orderDetails['items'] = array();
+        // if families are enabled, get all items
+        if($this->helper->getConfig('general/families')){
+            $items = $order->getAllItems();
+        } else {
+            $items = $order->getAllVisibleItems();
+        }
+        foreach ($items as $itemId => $item)
+        {
+            $product = $this->helper->getReviewableProductFromOrderItem($item);
+            /** @var \Magento\Catalog\Model\Product $product */
+            $product = $this->objectManager->get('\Magento\Catalog\Model\Product')->load($product->getId());
+            // skip configurable items if families are enabled
+            if($this->helper->getConfig('general/families') && $product->getTypeId() == \Magento\ConfigurableProduct\Model\Product\Type\Configurable::TYPE_CODE) continue;
+
+            $itemDetails = array();
+            $itemDetails['sku'] = $this->helper->getProductId($product);
+            $itemDetails['name'] = $item->getName();
+            // 'category' is not included.  Mage products can be in 0 - many categories.  Should we try to include it?
+            $itemDetails['price'] = number_format($item->getPrice(), 2, '.', '');
+            $itemDetails['quantity'] = number_format($item->getQtyOrdered(), 0);
+            $itemDetails['imageUrl'] = $product->getStore()->getUrl() . 'pub/media/catalog/product' . $product->getImage();
+
+            if($this->helper->getConfig('general/families') && $item->getParentItem()) {
+                if(strpos($itemDetails['imageUrl'], "placeholder/image.jpg")) {
+                    // if product families are enabled and product has no image, use configurable image
+                    $parentId = $item->getParentItem()->getProductId();
+                    /** @var \Magento\Catalog\Model\Product $parent */
+                    $parent = $this->objectManager->get('\Magento\Catalog\Model\Product')->load($parentId);
+                    $itemDetails['imageUrl'] = $parent->getStore()->getUrl() . 'pub/media/catalog/product' . $parent->getImage();
+                }
+                // also get price from parent item
+                $itemDetails['price'] = number_format($item->getParentItem()->getPrice(), 2, '.', '');
+            }
+
+            array_push($orderDetails['items'], $itemDetails);
+        }
+
+        if($order->getCustomerId()) {
+            $userId = $order->getCustomerId();
+        } else {
+            $userId = md5($order->getCustomerEmail());
+        }
+        $orderDetails['userId'] = $userId;
+        $orderDetails['email'] = $order->getCustomerEmail();
+        $orderDetails['nickname'] = $order->getCustomerFirstname();
+        // There is no 'deliveryDate' yet
+        $orderDetails['locale'] = $this->helper->getConfig('general/locale', $order->getStoreId());
+
+        // Add partnerSource field
+        $orderDetails['partnerSource'] = 'Magento Extension r' . $this->helper->getExtensionVersion();
+
+        $result .= '<pre>'.print_r($orderDetails, 1).'</pre>';
+
+        return $result;
+    }
+
+}
