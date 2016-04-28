d328006e7c0c22dddeed35441019652f0c8b364e
Dennis Rogers <dennis@storefrontconsulting.com>
2016-04-28 16:02:33 -0400

    BVEXT-160 #comment Modified setup to create product attribute.

--- app/code/Bazaarvoice/Connector/Setup/InstallData.php
+++ app/code/Bazaarvoice/Connector/Setup/InstallData.php
@@ -16,26 +16,32 @@ use Magento\Framework\Setup;
 use Bazaarvoice\Connector;
 use Magento\Sales\Setup\SalesSetup;
 use Magento\Sales\Setup\SalesSetupFactory;
+use Magento\Eav\Setup\EavSetup;
+use Magento\Eav\Setup\EavSetupFactory;
 use Magento\Sales\Model\Order;
+use Magento\Catalog\Model\Product;
+
 
 class InstallData implements Setup\InstallDataInterface
 {
-    /**
-     * Sales setup factory
-     *
-     * @var SalesSetupFactory
-     */
+    /** @var SalesSetupFactory */
     protected $salesSetupFactory;
+
+    /** @var EavSetup */
+    protected $eavSetupFactory;
 
     /**
      * Init
      *
      * @param SalesSetupFactory $salesSetupFactory
+     * @param EavSetupFactory $eavSetupFactory
      */
     public function __construct(
-        SalesSetupFactory $salesSetupFactory
+        SalesSetupFactory $salesSetupFactory,
+        EavSetupFactory $eavSetupFactory
     ) {
         $this->salesSetupFactory = $salesSetupFactory;
+        $this->eavSetupFactory = $eavSetupFactory;
     }
 
 
@@ -50,12 +56,39 @@ class InstallData implements Setup\InstallDataInterface
         $eavSetup->addAttribute(
             Order::ENTITY,
             Connector\Model\Feed\PurchaseFeed::ALREADY_SENT_IN_FEED_FLAG,
-            array(
+            [
                 'type' =>  \Magento\Framework\DB\Ddl\Table::TYPE_INTEGER,
                 'visible' => false,
                 'required' => false,
                 'default' => 0
-            )
+            ]
+        );
+
+        /** @var EavSetup $eavSetup */
+        $eavSetup = $this->eavSetupFactory->create(['setup' => $setup]);
+
+        $eavSetup->addAttribute(
+            Product::ENTITY,
+            Connector\Model\Feed\ProductFeed::INCLUDE_IN_FEED_FLAG,
+            [
+                'group' => 'Product Details',
+                'type' =>  'int',
+                'frontend' => '',
+                'label' => 'Send in Bazaarvoice Product Feed',
+                'input' => 'select',
+                'class' => '',
+                'source' => 'Magento\Catalog\Model\Product\Attribute\Source\Status',
+                'global' => \Magento\Eav\Model\Entity\Attribute\ScopedAttributeInterface::SCOPE_STORE,
+                'visible' => true,
+                'required' => false,
+                'user_defined' => true,
+                'default' => \Magento\Catalog\Model\Product\Attribute\Source\Status::STATUS_ENABLED,
+                'apply_to' => '',
+                'visible_on_front' => false,
+                'is_used_in_grid' => true,
+                'is_visible_in_grid' => false,
+                'is_filterable_in_grid' => false,
+            ]
         );
 
     }
