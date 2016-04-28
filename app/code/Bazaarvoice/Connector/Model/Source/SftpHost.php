09108627089524ba02583d89f90d16b05e830ffd
Dennis Rogers <dennis@storefrontconsulting.com>
2016-04-28 18:38:56 -0400

    BVEXT-154 #commit Replaced open hostname field with dropdown.

---
+++ app/code/Bazaarvoice/Connector/Model/Source/SftpHost.php
@@ -0,0 +1,22 @@
+<?php
+namespace Bazaarvoice\Connector\Model\Source;
+
+class SftpHost
+{
+    const BASE = 'sftp';
+    const EUROPE = 'sftp7';
+
+    public function toOptionArray()
+    {
+        return array(
+            array(
+                'value' => self::BASE,
+                'label' => __('Non-European Clients (C1-C6)')
+            ),
+            array(
+                'value' => self::EUROPE,
+                'label' => __('European Clients (C7)')
+            ),
+        );
+    }
+}
