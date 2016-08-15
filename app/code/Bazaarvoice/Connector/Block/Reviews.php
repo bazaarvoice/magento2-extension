dd180d4e4355a587b0eb6df7407632e6d2fd3de2
Dennis Rogers <dennis@storefrontconsulting.com>
2016-08-15 15:35:07 -0400

    BVEXT-196 #comment Add SEO aggregate ratings to product view.

--- app/code/Bazaarvoice/Connector/Block/Reviews.php
+++ app/code/Bazaarvoice/Connector/Block/Reviews.php
@@ -16,60 +16,96 @@ namespace Bazaarvoice\Connector\Block;
 use Bazaarvoice\BV;
 
 class Reviews extends Product
-{   
+{
+    /**
+     * Get only aggregate data
+     * @return string
+     */
+    public function getAggregateSEOContent()
+    {
+        if($this->getIsEnabled()){
+            $params = $this->_getParams();
+            $bv = new BV($params);
+            $seoContent = $bv->reviews->getAggregateRating();
+            $seoContent .= '
+<!-- BV Aggregate Rating Parameters: ' . print_r($params, 1) . '-->';
+            return $seoContent;
+        }
+        return '';
+    }
 
+    /**
+     * Get complete BV SEO Content
+     * @return string
+     */
     public function getSEOContent()
     {
-        $seoContent = '';
-        if($this->getConfig('general/enable_cloud_seo') && $this->isEnabled()) {
-            // Check if admin has configured a legacy display code
-            if(strlen($this->getConfig('bv_config/display_code'))) {
-                $deploymentZoneId =
-                    $this->getConfig('bv_config/display_code') .
-                    '-' . $this->getConfig('general/locale');
-            }
-            else {
-                $deploymentZoneId =
-                    str_replace(' ', '_', $this->getConfig('general/deployment_zone')) .
-                    '-' . $this->getConfig('general/locale');
-            }
-
-            $product = $this->getProduct();
-
-            $urlInterface = \Magento\Framework\App\ObjectManager::getInstance()->get('Magento\Framework\UrlInterface');
-            $productUrl = $urlInterface->getCurrentUrl();
-            $parts = parse_url($productUrl);
-            if(isset($parts['query'])) {
-                parse_str($parts['query'], $query);
-                unset($query['bvrrp']);
-                $baseUrl = $parts['scheme'] . '://' . $parts['host'] . $parts['path'] . '?' . http_build_query($query);
-            } else {
-                $baseUrl = $productUrl;
-            }
-
-            $this->_logger->addDebug($baseUrl);
-
-            $params = array(
-                'seo_sdk_enabled' => TRUE,
-                'bv_root_folder' => $deploymentZoneId, // replace with your display code (BV provided)
-                'subject_id' => $this->getHelper()->getProductId($product), // replace with product id 
-                'cloud_key' => $this->getConfig('general/cloud_seo_key'), // BV provided value
-                'base_url' => $baseUrl,
-                'page_url' => $productUrl,
-                'staging' => ($this->getConfig('general/environment') == "staging" ? TRUE : FALSE)
-            );
-            
-            
-            $requestInterface = \Magento\Framework\App\ObjectManager::getInstance()->get('Magento\Framework\App\RequestInterface');
-            if($requestInterface->getParam('bvreveal') == 'debug')
-                $params['bvreveal'] = 'debug';
-            
+        if($this->getIsEnabled()){
+            $params = $this->_getParams();
             $bv = new BV($params);
             $seoContent = $bv->reviews->getContent();
-            $seoContent .= '<!-- BV Reviews Parameters: ' . print_r($params, 1) . '-->';
+            $seoContent .= '
+<!-- BV Reviews SEO Parameters: ' . print_r($params, 1) . '-->';
+            return $seoContent;
         }
-        
-        return $seoContent;
+        return '';
+    }
+
+    /**
+     * Get BV SEO params from admin
+     * @return array
+     */
+    protected function _getParams()
+    {
+        // Check if admin has configured a legacy display code
+        if(strlen($this->getConfig('bv_config/display_code'))) {
+            $deploymentZoneId =
+                $this->getConfig('bv_config/display_code') .
+                '-' . $this->getConfig('general/locale');
+        }
+        else {
+            $deploymentZoneId =
+                str_replace(' ', '_', $this->getConfig('general/deployment_zone')) .
+                '-' . $this->getConfig('general/locale');
+        }
+
+        $product = $this->getProduct();
+
+        $urlInterface = \Magento\Framework\App\ObjectManager::getInstance()->get('Magento\Framework\UrlInterface');
+        $productUrl = $urlInterface->getCurrentUrl();
+        $parts = parse_url($productUrl);
+        if(isset($parts['query'])) {
+            parse_str($parts['query'], $query);
+            unset($query['bvrrp']);
+            $baseUrl = $parts['scheme'] . '://' . $parts['host'] . $parts['path'] . '?' . http_build_query($query);
+        } else {
+            $baseUrl = $productUrl;
+        }
+
+        $this->logger->debug($baseUrl);
+
+        $params = array(
+            'seo_sdk_enabled' => TRUE,
+            'bv_root_folder' => $deploymentZoneId, // replace with your display code (BV provided)
+            'subject_id' => $this->getHelper()->getProductId($product), // replace with product id
+            'cloud_key' => $this->getConfig('general/cloud_seo_key'), // BV provided value
+            'base_url' => $baseUrl,
+            'page_url' => $productUrl,
+            'staging' => ($this->getConfig('general/environment') == "staging" ? TRUE : FALSE)
+        );
+
+
+        $requestInterface = \Magento\Framework\App\ObjectManager::getInstance()->get('Magento\Framework\App\RequestInterface');
+        if($requestInterface->getParam('bvreveal') == 'debug')
+            $params['bvreveal'] = 'debug';
+
+        return $params;
+
+    }
+
+    protected function getIsEnabled()
+    {
+        return $this->getConfig('general/enable_cloud_seo') && $this->isEnabled();
     }
     
 }
