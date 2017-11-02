990eb58f700aa033838ce6bf7d529f964c7b175b
Dane MacMillan <work@danemacmillan.com>
2017-11-02 14:03:17 -0400

    #13 Fixes inconsistency in generating legal product IDs between the Magento1 and Magento2 module. This ensures that data will continue to get pulled into Magento2 stores when the product ID has a perfectly legal period in it.

--- Helper/Data.php
+++ Helper/Data.php
@@ -106,7 +106,7 @@ class Data extends AbstractHelper
             $rawProductId = $product;
 
         /** Customizations go here */
-        $rawProductId = preg_replace_callback('/\./s', create_function('$match', 'return "_bv".ord($match[0])."_";'), $rawProductId);
+
         /** No further customizations after this */
 
         return $this->replaceIllegalCharacters($rawProductId);
@@ -131,7 +131,7 @@ class Data extends AbstractHelper
          * Example encoded = qwerty_bv36__bv37__bv64__bv35_asdf
          */
 
-        return preg_replace_callback('/[^\w\d\*-\-_]/s', create_function('$match', 'return "_bv".ord($match[0])."_";'), $rawId);
+        return preg_replace_callback('/[^\w\d\*-\-\._]/s', create_function('$match', 'return "_bv".ord($match[0])."_";'), $rawId);
     }
 
 
