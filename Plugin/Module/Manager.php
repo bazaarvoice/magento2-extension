627fe8db9e4ddad8dfa92db19705d65c32e126c5
Dennis Rogers <dennis@storefrontconsulting.com>
2018-03-26 15:54:59 -0400

    Brute force disable module output for Magento_Reviews.

---
+++ Plugin/Module/Manager.php
@@ -0,0 +1,35 @@
+<?php
+/**
+ * StoreFront Bazaarvoice Extension for Magento
+ *
+ * PHP Version 5
+ *
+ * LICENSE: This source file is subject to commercial source code license
+ * of StoreFront Consulting, Inc.
+ *
+ * @category  SFC
+ * @package   bvmage2
+ * @author    Dennis Rogers <dennis@storefrontconsulting.com>
+ * @copyright 2018 StoreFront Consulting, Inc
+ * @license   http://www.storefrontconsulting.com/media/downloads/ExtensionLicense.pdf StoreFront Consulting Commercial License
+ * @link      http://www.StoreFrontConsulting.com/bazaarvoice-extension/
+ */
+
+namespace Bazaarvoice\Connector\Plugin\Module;
+
+class Manager extends \Magento\Framework\Module\Manager {
+
+	/**
+	 * @param string $moduleName
+	 *
+	 * @return bool
+	 */
+	public function isOutputEnabled( $moduleName ) {
+
+		if($moduleName == 'Magento_Review') return false;
+
+		return parent::isOutputEnabled( $moduleName );
+	}
+
+
+}
