dc155efdd4be372e54f71443eea2d43aead86400
Jantzen Ward <jward@degdigital.com>
2019-02-11 13:03:07 -0600

    MGP-889: Fix Product Feed XML > Product > CategoryExternalID node sometimes including slash characters if "Use category URL path as category id" is set to No.
    This seems to be possible when a product has the "Bazaarvoice Category" product attribute set, which will include slash characters, and the "Use category URL path as category id" configuration is set to No, causing bad characters to not be cleaned.

--- Model/Indexer/Flat.php
+++ Model/Indexer/Flat.php
@@ -579,11 +579,9 @@ class Flat implements \Magento\Framework\Indexer\ActionInterface, \Magento\Frame
                 $indexData['parent_category_external_id'] = $indexData['bv_parent_category_external_id'];
             }
 
-            if ( $this->_helper->getConfig( 'feeds/category_id_use_url_path', $storeId ) ) {
-                $indexData['category_external_id'] = str_replace( '/', '-', $indexData['category_external_id'] );
-                $indexData['category_external_id'] = str_replace( '.html', '', $indexData['category_external_id'] );
-                $indexData['category_external_id'] = $this->_helper->replaceIllegalCharacters( $indexData['category_external_id'] );
-            }
+            $indexData['category_external_id'] = str_replace( '/', '-', $indexData['category_external_id'] );
+            $indexData['category_external_id'] = str_replace( '.html', '', $indexData['category_external_id'] );
+            $indexData['category_external_id'] = $this->_helper->replaceIllegalCharacters( $indexData['category_external_id'] );
 
             /** Check locales */
             $productLocales = [];
