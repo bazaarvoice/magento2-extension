44f4cda4cf1d8c658043fc221f9e6a4384a1586e
Jantzen Ward <jward@degdigital.com>
2019-03-13 13:53:45 -0500

    MGP-898: Fix pixel not displaying on the checkout success page if the 'Magento\Checkout\Block\Registration' block was removed

--- Block/Pixel.php
+++ Block/Pixel.php
@@ -1,162 +1,154 @@
 <?php
-/**
- * StoreFront Bazaarvoice Extension for Magento
- *
- * PHP Version 5
- *
- * LICENSE: This source file is subject to commercial source code license
- * of StoreFront Consulting, Inc.
- *
- * @category  SFC
- * @package   Bazaarvoice_Ext
- * @author    Dennis Rogers <dennis@storefrontconsulting.com>
- * @copyright 2016 StoreFront Consulting, Inc
- * @license   http://www.storefrontconsulting.com/media/downloads/ExtensionLicense.pdf StoreFront Consulting Commercial License
- * @link      http://www.StoreFrontConsulting.com/bazaarvoice-extension/
- */
 
 namespace Bazaarvoice\Connector\Block;
 
-use \Magento\ConfigurableProduct\Model\Product\Type\Configurable;
+use Bazaarvoice\Connector\Helper\Data;
+use Magento\Catalog\Helper\Image;
+use Magento\Catalog\Model\Product\Visibility;
+use Magento\Catalog\Model\ProductRepository;
+use Magento\Checkout\Model\Session;
+use Magento\ConfigurableProduct\Model\Product\Type\Configurable;
+use Magento\Directory\Model\Region;
 use Magento\Framework\Exception\NoSuchEntityException;
+use Magento\Framework\View\Element\Template;
+use Magento\Framework\View\Element\Template\Context;
 
-class Pixel {
+class Pixel extends Template
+{
     protected $helper;
     protected $checkoutSession;
     protected $imageHelper;
     protected $region;
-    protected $productRepo;
+    protected $productRepository;
+    private $orderDetails;
 
     /**
-     * @param \Bazaarvoice\Connector\Helper\Data $helper
-     * @param \Magento\Checkout\Model\Session $checkoutSession
-     * @param \Magento\Catalog\Helper\Image $imageHelper
-     * @param \Magento\Directory\Model\Region $region
-     * @param \Magento\Catalog\Model\ProductRepository $productRepo
+     * Pixel constructor.
+     *
+     * @param \Magento\Framework\View\Element\Template\Context $context
+     * @param \Bazaarvoice\Connector\Helper\Data               $helper
+     * @param \Magento\Catalog\Helper\Image                    $imageHelper
+     * @param \Magento\Checkout\Model\Session                  $checkoutSession
+     * @param \Magento\Directory\Model\Region                  $region
+     * @param \Magento\Catalog\Model\ProductRepository         $productRepo
+     * @param array                                            $data
      */
     public function __construct(
-        \Bazaarvoice\Connector\Helper\Data $helper,
-        \Magento\Checkout\Model\Session $checkoutSession,
-        \Magento\Catalog\Helper\Image $imageHelper,
-        \Magento\Directory\Model\Region $region,
-        \Magento\Catalog\Model\ProductRepository $productRepo
+        Context $context,
+        Data $helper,
+        Image $imageHelper,
+        Session $checkoutSession,
+        Region $region,
+        ProductRepository $productRepo,
+        array $data = []
     ) {
-        $this->helper          = $helper;
+        $this->helper = $helper;
         $this->checkoutSession = $checkoutSession;
-        $this->imageHelper     = $imageHelper;
-        $this->region          = $region;
-        $this->productRepo     = $productRepo;
+        $this->imageHelper = $imageHelper;
+        $this->region = $region;
+        $this->productRepository = $productRepo;
+        parent::__construct($context, $data);
     }
 
-    /** @codingStandardsIgnoreStart
-     * @param \Magento\Checkout\Block\Onepage\Success $subject
-     * @param $result
-     *
-     * @return string
-     */
-    public function afterToHtml(
-        /** @noinspection PhpUnusedParameterInspection */
-        $subject, $result
-    ) {
-        /** @codingStandardsIgnoreEnd */
-        if ( $this->helper->getConfig( 'general/enable_bvpixel' ) != true ) {
-            return $result;
-        }
+    public function getBvPixelEnabled()
+    {
+        return $this->helper->getConfig('general/enable_bvpixel');
+    }
 
+    public function getJsonOrderDetails()
+    {
+        return json_encode($this->getOrderDetails(), JSON_UNESCAPED_UNICODE);
+    }
+
+    public function getOrderDetails()
+    {
         /** @var \Magento\Sales\Model\Order $order */
         $order = $this->checkoutSession->getLastRealOrder();
 
-        $orderDetails = array();
+        if ($this->orderDetails) {
+            return $this->orderDetails;
+        }
 
         $address = $order->getBillingAddress();
 
-        $orderDetails['currency'] = $order->getOrderCurrencyCode();
-        $orderDetails['orderId']  = $order->getIncrementId();
+        $this->orderDetails['currency'] = $order->getOrderCurrencyCode();
+        $this->orderDetails['orderId'] = $order->getIncrementId();
 
-        $total                    = $order->getGrandTotal() - $order->getTaxAmount() - $order->getShippingAmount();
-        $orderDetails['total']    = number_format( $total, 2, '.', '' );
-        $orderDetails['tax']      = number_format( $order->getTaxAmount(), 2, '.', '' );
-        $orderDetails['shipping'] = number_format( $order->getShippingAmount(), 2, '.', '' );
+        $total = $order->getGrandTotal() - $order->getTaxAmount() - $order->getShippingAmount();
+        $this->orderDetails['total'] = number_format($total, 2, '.', '');
+        $this->orderDetails['tax'] = number_format($order->getTaxAmount(), 2, '.', '');
+        $this->orderDetails['shipping'] = number_format($order->getShippingAmount(), 2, '.', '');
 
-        $orderDetails['city']    = $address->getCity();
-        $orderDetails['state']   = $this->region->load( $address->getRegionId() )->getCode();
-        $orderDetails['country'] = $address->getCountryId();
+        $this->orderDetails['city'] = $address->getCity();
+        $this->orderDetails['state'] = $this->region->load($address->getRegionId())->getCode();
+        $this->orderDetails['country'] = $address->getCountryId();
 
-        $orderDetails['items'] = array();
+        $this->orderDetails['items'] = array();
         /** if families are enabled, get all items */
-        if ( $this->helper->getConfig( 'general/families' ) ) {
+        if ($this->helper->getConfig('general/families')) {
             $items = $order->getAllItems();
         } else {
             $items = $order->getAllVisibleItems();
         }
-        foreach ( $items as $itemId => $item ) {
+        foreach ($items as $itemId => $item) {
             try {
-                $product = $this->getReviewableProductFromOrderItem( $item );
-            } catch ( NoSuchEntityException $e ) {
+                $product = $this->getReviewableProductFromOrderItem($item);
+            } catch (NoSuchEntityException $e) {
                 continue;
             }
             /** skip configurable items if families are enabled */
             if (
-                $this->helper->getConfig( 'general/families' )
-                && $product->getTypeId() == Configurable::TYPE_CODE ) {
+                $this->helper->getConfig('general/families')
+                && $product->getTypeId() == Configurable::TYPE_CODE) {
                 continue;
             }
 
-            $itemDetails        = array();
-            $itemDetails['sku'] = $this->helper->getProductId( $product );
+            $itemDetails = array();
+            $itemDetails['sku'] = $this->helper->getProductId($product);
 
             $itemDetails['name'] = $item->getName();
             /** 'category' is not included.  Mage products can be in 0 - many categories.  Should we try to include it? */
-            $itemDetails['price']    = number_format( $item->getPrice(), 2, '.', '' );
-            $itemDetails['quantity'] = number_format( $item->getQtyOrdered(), 0 );
-            $itemDetails['imageURL'] = $this->imageHelper->init( $product, 'product_small_image' )->setImageFile( $product->getSmallImage() )->getUrl();
+            $itemDetails['price'] = number_format($item->getPrice(), 2, '.', '');
+            $itemDetails['quantity'] = number_format($item->getQtyOrdered(), 0);
+            $itemDetails['imageURL'] = $this->imageHelper->init($product, 'product_small_image')
+                ->setImageFile($product->getSmallImage())->getUrl();
 
-            if ( $this->helper->getConfig( 'general/families' ) && $item->getParentItem() ) {
-                if ( strpos( $itemDetails['imageURL'], 'placeholder/image.jpg' ) ) {
+            if ($this->helper->getConfig('general/families') && $item->getParentItem()) {
+                if (strpos($itemDetails['imageURL'], 'placeholder/image.jpg')) {
                     /** if product families are enabled and product has no image, use configurable image */
                     $parentId = $item->getParentItem()->getProductId();
                     try {
-                        $parent                  = $this->productRepo->getById( $parentId );
-                        $itemDetails['imageURL'] = $this->imageHelper->init( $parent, 'product_small_image' )->setImageFile( $parent->getSmallImage() )->getUrl();
-                    } catch ( NoSuchEntityException $e ) {
+                        $parent = $this->productRepository->getById($parentId);
+                        $itemDetails['imageURL'] = $this->imageHelper->init($parent, 'product_small_image')
+                            ->setImageFile($parent->getSmallImage())->getUrl();
+                    } catch (NoSuchEntityException $e) {
                     }
                 }
                 /** also get price from parent item */
-                $itemDetails['price'] = number_format( $item->getParentItem()->getPrice(), 2, '.', '' );
+                $itemDetails['price'] = number_format($item->getParentItem()->getPrice(), 2, '.', '');
             }
 
-            array_push( $orderDetails['items'], $itemDetails );
+            array_push($this->orderDetails['items'], $itemDetails);
         }
-        if ( $order->getCustomerId() ) {
+        if ($order->getCustomerId()) {
             $userId = $order->getCustomerId();
         } else {
-            $userId = md5( $order->getCustomerEmail() );
+            $userId = md5($order->getCustomerEmail());
         }
-        $orderDetails['userId']   = $userId;
-        $orderDetails['email']    = $order->getCustomerEmail();
-        $orderDetails['nickname'] = $order->getCustomerFirstname()
+        $this->orderDetails['userId'] = $userId;
+        $this->orderDetails['email'] = $order->getCustomerEmail();
+        $this->orderDetails['nickname'] = $order->getCustomerFirstname()
             ? $order->getCustomerFirstname()
             : $order->getBillingAddress()->getFirstname();
         /** There is no 'deliveryDate' yet */
-        $orderDetails['locale'] = $this->helper->getConfig( 'general/locale', $order->getStoreId() );
+        $this->orderDetails['locale'] = $this->helper->getConfig('general/locale', $order->getStoreId());
 
         /** Add partnerSource field */
-        $orderDetails['partnerSource']  = 'Magento Extension r' . $this->helper->getExtensionVersion();
-        $orderDetails['deploymentZone'] = strtolower( str_replace( ' ', '_', $this->helper->getConfig( 'general/deployment_zone' ) ) );
+        $this->orderDetails['partnerSource'] = 'Magento Extension r'.$this->helper->getExtensionVersion();
+        $this->orderDetails['deploymentZone'] = strtolower(str_replace(' ', '_',
+            $this->helper->getConfig('general/deployment_zone')));
 
-        if ( $this->helper->getConfig( 'general/environment' ) == 'staging' ) {
-            $result .= '
-	        <!--
-	        ' . print_r( $orderDetails, 1 ) . '
-	        -->';
-        }
-        $result .= '
-        <script type="text/javascript">
-            var transactionData = ' . json_encode( $orderDetails, JSON_UNESCAPED_UNICODE ) . ';
-            BV.pixel.trackTransaction(transactionData);
-        </script>';
-
-        return $result;
+        return $this->orderDetails;
     }
 
     /**
@@ -169,20 +161,32 @@ class Pixel {
      * @return \Magento\Catalog\Model\Product
      * @throws NoSuchEntityException
      */
-    public function getReviewableProductFromOrderItem( $item ) {
-        $product = $this->productRepo->getById( $item->getProductId() );
-        $product->setStoreId( $item->getStoreId() );
+    public function getReviewableProductFromOrderItem($item)
+    {
+        $product = $this->productRepository->getById($item->getProductId());
+        $product->setStoreId($item->getStoreId());
 
-        if ( $product->getVisibility() == \Magento\Catalog\Model\Product\Visibility::VISIBILITY_NOT_VISIBLE ) {
+        if ($product->getVisibility() == Visibility::VISIBILITY_NOT_VISIBLE) {
             $options = $item->getProductOptions();
-            if ( isset( $options['super_product_config']['product_id'] ) ) {
+            if (isset($options['super_product_config']['product_id'])) {
                 try {
                     $parentId = $options['super_product_config']['product_id'];
-                    $product  = $this->productRepo->getById( $parentId );
-                } catch ( \Exception $ex ) { }
+                    $product = $this->productRepository->getById($parentId);
+                } catch (\Exception $ex) {
+                }
             }
         }
 
         return $product;
+    }
+
+    public function canShowDebugDetails()
+    {
+        return ($this->helper->getConfig('general/environment') == 'staging');
+    }
+
+    public function getDebugDetails()
+    {
+        return print_r($this->getOrderDetails(), true);
     }
 }
