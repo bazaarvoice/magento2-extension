5c2a0a80e7469f4156a2f4878da9534eaedf8f8c
Ben Robie <brobie@degdigital.com>
2019-05-02 15:32:38 -0500

    Removing license block in preparation for new license definition.

--- Block/Disable.php
+++ Block/Disable.php
@@ -1,19 +1,4 @@
 <?php
-/**
- * StoreFront Bazaarvoice Extension for Magento
- *
- * PHP Version 5
- *
- * LICENSE: This source file is subject to commercial source code license
- * of StoreFront Consulting, Inc.
- *
- * @category  SFC
- * @package   bvmage2
- * @author    Dennis Rogers <dennis@storefrontconsulting.com>
- * @copyright 2018 StoreFront Consulting, Inc
- * @license   http://www.storefrontconsulting.com/media/downloads/ExtensionLicense.pdf StoreFront Consulting Commercial License
- * @link      http://www.StoreFrontConsulting.com/bazaarvoice-extension/
- */
 
 namespace Bazaarvoice\Connector\Block;
 
--- Ui/Component/Listing/Column/Json.php
+++ Ui/Component/Listing/Column/Json.php
@@ -1,22 +1,6 @@
 <?php
-/**
- * StoreFront Bazaarvoice Extension for Magento
- *
- * PHP Version 5
- *
- * LICENSE: This source file is subject to commercial source code license
- * of StoreFront Consulting, Inc.
- *
- * @category  SFC
- * @package   Bazaarvoice_Ext
- * @author    Dennis Rogers <dennis@storefrontconsulting.com>
- * @copyright 2016 StoreFront Consulting, Inc
- * @license   http://www.storefrontconsulting.com/media/downloads/ExtensionLicense.pdf StoreFront Consulting Commercial License
- * @link      http://www.StoreFrontConsulting.com/bazaarvoice-extension/
- */
 
 namespace Bazaarvoice\Connector\Ui\Component\Listing\Column;
-
 
 use Bazaarvoice\Connector\Helper\Data;
 use Magento\Framework\View\Element\UiComponent\ContextInterface;
