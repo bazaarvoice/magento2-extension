d20a485c664566832939e994e830e5393f82c01f
Ben Robie <brobie@degdigital.com>
2019-05-02 15:32:38 -0500

    Removing license block in preparation for new license definition.

--- Console/Command/Product.php
+++ Console/Command/Product.php
@@ -1,19 +1,4 @@
 <?php
-/**
- * StoreFront Bazaarvoice Extension for Magento
- *
- * PHP Version 5
- *
- * LICENSE: This source file is subject to commercial source code license
- * of StoreFront Consulting, Inc.
- *
- * @category  SFC
- * @package   Bazaarvoice_Ext
- * @author    Dennis Rogers <dennis@storefrontconsulting.com>
- * @copyright 2016 StoreFront Consulting, Inc
- * @license   http://www.storefrontconsulting.com/media/downloads/ExtensionLicense.pdf StoreFront Consulting Commercial License
- * @link      http://www.StoreFrontConsulting.com/bazaarvoice-extension/
- */
 
 namespace Bazaarvoice\Connector\Console\Command;
  
