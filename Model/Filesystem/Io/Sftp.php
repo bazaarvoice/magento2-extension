9e9668c6843a40053fdb761e430c22c674a6bf00
Pavel Shulga <pavel@shulga.io>
2022-11-02 11:04:36 +1100

    BV Logger err method issue * \phpseclib\Net\SFTP is deprecated in Magento 2.4.*

--- Model/Filesystem/Io/Sftp.php
+++ Model/Filesystem/Io/Sftp.php
@@ -13,23 +13,4 @@ namespace Bazaarvoice\Connector\Model\Filesystem\Io;
  */
 class Sftp extends \Magento\Framework\Filesystem\Io\Sftp
 {
-    /**
-     * @var \phpseclib\Net\SFTP $_connection
-     */
-    protected $_connection = null;
-
-    /**
-     * Fixes Magento 2.1 phpseclib notice.
-     *
-     * @param $filename
-     * @param $source
-     * @param null $mode
-     *
-     * @return mixed
-     */
-    public function write($filename, $source, $mode = null)
-    {
-        $mode = is_readable($source) ? \phpseclib\Net\SFTP::SOURCE_LOCAL_FILE : \phpseclib\Net\SFTP::SOURCE_STRING;
-        return $this->_connection->put($filename, $source, $mode);
-    }
 }
